cda07bba752e9c43970ab94e66ec6fa9720fd398
Stephen Greene <sgreene570@gmail.com>
2017-05-24 16:41:35 -0400

    initial function declarations

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SortedDictADT
 My implementation of a sorted dictionary using generics in C.
+Uses a linked list of dictionary entries instead of a hash based array, so the term "dictionary" is used loosely.
+
--- SortedDictADT.h
+++ SortedDictADT.h
@@ -1,3 +1,52 @@
 // File: SortedDictADT.h
 // Sorted dictionary adt header file 
-// @author: Stephen Greene
+// @author: Stephen Greene (sgreene570)
+
+
+#ifndef SORTEDDICTADT_H
+#define SORTEDDICTADT_H
+
+
+// Struct SortedDictEntry
+// void *key : Key entry for dictionary entry
+// void *value : Value associated with given key
+// struct SortedDictEntry_s *next : Linked list behavior to point to next entry
+typedef SortedDictEntry_s{
+    void *key;
+    void *value;
+    struct SortedDictEntry_s *next;
+}*SortedDictEntry;
+
+
+// Struct SortedDictADT
+// int (*cmp)(const void *a, const void *b) : comparator function pointer
+// SortedDictEntry head : Head node of the dictionary
+typedef SortedDictADT_s{
+    int (*cmp)(const void *a, const void *b);
+    SortedDictEntry head;
+}*SortedDictADT;
+
+
+// createDict()
+// Creates and returns a SortedDictADT
+// @param int (*cmp)(const void *a, const void *b) : comparator function
+// @return SortedDictADT : Blank SortedDictADT instance
+SortedDictADT createDict(int (*cmp)(const void *a, const void *b));
+
+
+// createEntry()
+// Creates a dictionary entry struct and returns a struct pointer
+// @param void *key : Key for dict entry
+// @param void *value : Value for associated key
+// @return SortedDictEntry : Pointer to dict entry
+SortedDictEntry createEntry(void *key, void *value)
+
+
+// addEntry()
+// Add a SortedDictEntry to the passed SortedDictADT
+// @param SortedDictADT dict: The dict to add the entry to
+// @param SortedDictEntry entry : The entry to add
+void addEntry(SortedDictADT dict, SortedDictEntry entry)
+
+
+#endif
